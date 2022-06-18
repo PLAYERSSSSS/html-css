e14af80dcd01dff29de48471ef7c2e62b1f15de8
PLAYERSSSSS <102339172+PLAYERSSSSS@users.noreply.github.com>
2022-06-19 01:28:03 +0800

    Add files via upload
    MINecraft+html+css+json

---
+++ html+css/js/window.js
@@ -0,0 +1,16 @@
+document.getElementById('zhezhao').style.display = "none";
+function dianwo() {
+    document.getElementById('zhezhao').style.display = "";
+}
+function hidder() {
+    document.getElementById('zhezhao').style.display = "none";
+}
+function wx_pay() {
+    document.getElementById("pay_img").src="http://m.qpic.cn/psc?/V50h2bdj3bUv3V3gUJB73J6PY80kqBJr/ruAMsa53pVQWN7FLK88i5lp3*6fQ3tXNKmlQ4Cg3hkHQHmV6kKe2gY7AetyiYOsVKt1yGBlgal*ksvMp6Hdky9cNCu.ZN*UwBxyF1VGMKAw!/mnull&bo=uQK4AgAAAAABByE!&rf=photolist&t=5"
+}
+function zfb_pay() {
+    document.getElementById("pay_img").src="https://lablog-images.oss-cn-beijing.aliyuncs.com/images/pay.jpg"
+}
+function qq_pay() {
+    document.getElementById("pay_img").src="https://lablog-images.oss-cn-beijing.aliyuncs.com/images/QQ.png"
+}
